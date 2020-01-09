2dbc0881d723a070efff0847b22ba02f07e62c15
lihuan <1968763315@qq.com>
2020-01-10 00:12:12 +0800

    小程序首页基本完成

---
+++ pages/index/index.js
@@ -0,0 +1,125 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    currentIndex: 0,
+    navList:[],
+    swiperList:[],
+    videosList: [] 
+  },
+
+
+  activeNav(e){
+    this.setData({
+      currentIndex:e.target.dataset.index
+    })
+  },
+   /**
+    * 获取首页导航数据
+    */
+  getNavList(){
+    wx.request({
+      url:"https://mockapi.eolinker.com/7b7NMB9c75d613bc39c8f16e4e03a3d4a8f951750079dc5/navList",
+        success:(res)=>{
+        if(res.data.code === 0){
+          this.setData({
+            navList: res.data.data.navList
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 获取轮播图数据
+   */
+  getSwiperList(){
+    wx.request({
+      url: "https://mockapi.eolinker.com/7b7NMB9c75d613bc39c8f16e4e03a3d4a8f951750079dc5/Swiper",
+      success: (res)=>{
+        if(res.data.code === 0){
+          this.setData({
+            swiperList:res.data.data.swiperList
+          })
+        }
+      }
+    })
+  },
+  getVideosList(){
+    wx.request({
+      url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videosList ",
+      success: (res)=>{
+        console.log(res);
+        
+        if(res.data.code === 0){
+          this.setData({
+            videosList:res.data.data.videosList
+          })
+          
+        }
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      this.getNavList()
+      this.getSwiperList()
+      this.getVideosList()
+     
+  
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
